f6981983f33f8fb3f20a66565300b21e395024e3
Xun Yuan <yuanxun2000@outlook.com>
2026-04-28 19:38:10 -0400

    refactor: expand pipeline into steps

--- scalar.py
+++ scalar.py
@@ -2,8 +2,9 @@ import json
 import logging
 from pathlib import Path
 
+from peft import LoraConfig, get_peft_model
 from rich.progress import track
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from critic import get_response
 from prompt import compute_length_bounds
@@ -12,18 +13,41 @@ from utils import get_jinja_env
 logger = logging.getLogger(__name__)
 
 
-def loop(config: dict, run_path: Path) -> Path:
+def loop(config: dict, run_path: Path) -> None:
     env = get_jinja_env("prompts")
     artist_prompt = env.get_template("artist/init.jinja")
     critic_prompt_dev = env.get_template("critic/scalar/dev.jinja")
     critic_prompt_user = env.get_template("critic/scalar/user.jinja")
 
     artist_config = config["artist"]
-    pipe = pipeline(model=artist_config["model"])
+    model_name = artist_config["model"]
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+    if "lora" in artist_config:
+        lora_config = LoraConfig(**artist_config["lora"])
+        model = get_peft_model(model, lora_config)
 
     generate_config = artist_config["generate"]
     low, high = compute_length_bounds(generate_config["max_new_tokens"])
-    artist_prompt = artist_prompt.render(words_low=low, words_high=high)
+    artist_prompt = [
+        {
+            "role": "user",
+            "content": artist_prompt.render(words_low=low, words_high=high),
+        }
+    ]
+    artist_prompt = tokenizer.apply_chat_template(
+        artist_prompt,
+        return_tensors="pt",
+        add_generation_prompt=True,
+    ).to(model.device)
+    # remove prompt by string length, consistent with transformers.Pipeline behavior in textual mode
+    prompt_len = len(
+        tokenizer.decode(
+            artist_prompt.input_ids[0],
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=True,
+        )
+    )
 
     critic_config = config["critic"]
 
@@ -33,19 +57,20 @@ def loop(config: dict, run_path: Path) -> Path:
 
     exp_config = config["experiment"]
     for step in track(range(exp_config["num_steps"]), description="Looping..."):
-        model_output = pipe(
-            [{"role": "user", "content": artist_prompt}],
-            return_full_text=False,
-            **generate_config,
+        model_output = model.generate(**artist_prompt, **generate_config)
+        text = tokenizer.decode(
+            model_output,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=True,
         )
-        model_output = [o["generated_text"].lstrip() for o in model_output]
+        text = [t[prompt_len:].lstrip() for t in text]
 
         critic_rank = get_response(
             model=critic_config["model"],
             dev_input=critic_prompt_dev.render(
-                total=len(model_output), top_k=top_k, bottom_k=bottom_k
+                total=len(text), top_k=top_k, bottom_k=bottom_k
             ),
-            user_input=critic_prompt_user.render(texts=model_output),
+            user_input=critic_prompt_user.render(texts=text),
             max_retries=critic_config["max_retries"],
         )
 
